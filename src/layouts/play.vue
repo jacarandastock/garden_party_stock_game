7c434ac268cba48668e7f94a76aef5d96989033c
Gary Du <gudu9182@uni.sydney.edu.au>
2023-10-01 05:25:40 +1100

    init

--- src/layouts/play.vue
+++ src/layouts/play.vue
@@ -8,7 +8,7 @@ const {t, locale} = useI18n();
 const dataStore = useDataStore();
 const nextYearDialog = ref(false)
 const disabled = ref(false)
-
+const resetDialog = ref(false)
 function nextYear() {
   api.post('/api/stock/year/update').then((data) => {
     if (data.data.success) {
@@ -20,6 +20,13 @@ function nextYear() {
     }
   })
 }
+
+function reset() {
+  api.post('/api/reset').then((data) => {
+
+  })
+}
+const drawer = ref(false)
 const { name } = useDisplay();
 const isMobile = computed(() => name.value === 'xs');
 </script>
@@ -68,7 +75,7 @@ const isMobile = computed(() => name.value === 'xs');
         </v-card>
       </v-dialog>
       <v-spacer></v-spacer>
-      <v-btn icon variant="text">
+      <v-btn icon variant="text" @click="drawer=!drawer">
         <span class="i-iconoir-bell text-2xl"></span>
       </v-btn>
       <LocaleToggler/>
@@ -77,6 +84,49 @@ const isMobile = computed(() => name.value === 'xs');
 
     <v-main class="text-slate-700 dark:text-slate-300">
       <RouterView/>
+      <v-navigation-drawer
+          v-model="drawer"
+          location="bottom"
+          temporary
+      >
+        <v-list>
+          <v-list-item>
+            <v-dialog
+                v-model="resetDialog"
+                width="auto"
+            >
+              <template v-slot:activator="{ props }">
+                <v-btn v-show="$route.meta.isAdmin" v-bind="props" variant="elevated" class="ml-5" color="black" :disabled="disabled">
+                  重置数据
+                </v-btn>
+              </template>
+              <v-card>
+                <v-card-title>
+                  您确定要重置所有数据吗
+                </v-card-title>
+                <v-card-text>这个操作无法撤销, 会清除所有用户并将游戏重置为初始状态 </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                      color="primary"
+                      variant="elevated"
+                      @click="resetDialog = false"
+                  >
+                    放弃
+                  </v-btn>
+                  <v-btn
+                      color="error"
+                      variant="text"
+                      @click="reset(); resetDialog = false"
+                  >
+                    确定
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
     </v-main>
   </v-app>
 </template>
